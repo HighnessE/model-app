5d29e07a93dd1638a969417122adb93c943b701b
大帅比 <934780820@qq.com>
2017-11-13 18:14:39 +0800

    五图模板

--- src/components/CardChooseImages/CardChooseImages.vue
+++ src/components/CardChooseImages/CardChooseImages.vue
@@ -43,7 +43,7 @@
 					<a class="get-file" @click="useImage">使用</a>
 				</div>
 				<div class="main container">
-					<croppa v-model="myCroppa" :width="screenWidth * 28.5 / 100" :height="screenWidth * 419 / 1015 / 2" accept="image/*" :show-remove-button="false"></croppa>
+					<croppa v-model="myCroppa" :width="croppaWidth" :height="croppaHeight" accept="image/*" :zoom-speed="10" :quality="croppaQuality"></croppa>
 				</div>
 			</div>
 			<!--<croppa v-model="myCroppa1" :width="screenWidth * 28.5 / 100" :height="screenWidth * 419 / 1015 / 2" accept="image/png" :show-remove-button="false" @file-choose="handleCroppaFileChoose"></croppa>-->
@@ -52,20 +52,25 @@
 </template>
 <script>
 import { XHeader } from 'vux'
+import _ from 'lodash'
+import qs from 'qs'
+import { mapGetters } from 'vuex'
 export default {
 	components: {
 		XHeader
 	},
 	data() {
 		return {
-			// 屏幕宽度
-			screenWidth: 200,
+			// 正在编辑的 croppa 宽高和缩放比
+			croppaWidth: 200,
+			croppaHeight: 200,
+			croppaQuality: 1,
 			// croppa 绑定的数据对象
 			myCroppa: {},
 			// 是否允许提交到制作模卡接口
 			isAllowSubmitCard: false,
 			// 选中正在编辑的图片
-			imageEditing: '',
+			imageEditing: {},
 			// 是否展示编辑图片窗口
 			isShowEditPanel: false,
 			// 模板类型
@@ -76,65 +81,75 @@ export default {
 					imageId: '5-0',
 					isChecked: false,
 					initImage: require('./imgs/five-one.jpg'),
-					base64Image: ''
+					// base64Image: '',
+					imageChosen: false,
+					width: 894,
+					height: 1251,
+					aspectRatio: 298 / 417
 				},
 				{
 					imageId: '5-1',
 					isChecked: false,
 					initImage: require('./imgs/five-two.jpg'),
-					base64Image: ''
+					// base64Image: '',
+					imageChosen: false,
+					width: 891,
+					height: 624,
+					aspectRatio: 297 / 208
 				},
 				{
 					imageId: '5-2',
 					isChecked: false,
 					initImage: require('./imgs/five-two.jpg'),
-					base64Image: ''
+					// base64Image: '',
+					imageChosen: false,
+					width: 891,
+					height: 624,
+					aspectRatio: 297 / 208
 				},
 				{
 					imageId: '5-3',
 					isChecked: false,
 					initImage: require('./imgs/five-two.jpg'),
-					base64Image: ''
+					// base64Image: '',
+					imageChosen: false,
+					width: 891,
+					height: 624,
+					aspectRatio: 297 / 208
 				},
 				{
 					imageId: '5-4',
 					isChecked: false,
 					initImage: require('./imgs/five-two.jpg'),
-					base64Image: ''
+					// base64Image: '',
+					imageChosen: false,
+					width: 891,
+					height: 624,
+					aspectRatio: 297 / 208
 				},
 			]
 		}
 	},
 	methods: {
 		upLoadCard() {
-			this.myCroppa0.remove();
 			if (this.isAllowSubmitCard) {
-				// console.log(this.myCroppa0.generateDataUrl())
-				console.log(this.$route.params.template);
-				// axios({
-				// 	methods: 'POST',
-				// 	url: '/Work/joint',
-				// 	data: {
-				// 		type: this.$params.template ? this.$params.template : '',
-				// 		modelName: modelName,
-				// 		modelHeight: this.,
-				// 		modelWeight: modelWeight,
-				// 		modelBust: modelBust,
-				// 		modelWaist: modelWaist,
-				// 		modelHips: modelHips,
-				// 		modelShoes: modelShoes
-				// 	}
-				// });
+				this.$http.post('/model/Work/joint', qs.stringify({
+					type: this.templateType,
+					...this.modelCardDataGetter
+				})).then((response) => {
+					var res = response.data;
+					console.log(res);
+				});
 			} else {
 
 			}
 		},
-		// 选择需要编辑的图片方法
+		// 选择需要编辑的图片
 		chooseImageToEdit(templateArray, index) {
-			// 缓存需要编辑的图片id
-			this.imageEditing = templateArray[index].imageId;
+			// 缓存需要编辑的图片对象
+			this.imageEditing = templateArray[index];
 			// 改变选中状态
-			templateArray.forEach((item, idx)=>{
+			templateArray.forEach((item, idx) => {
 				if (idx == index) {
 					item.isChecked = true;
 				} else {
@@ -146,41 +161,111 @@ export default {
 		editImage() {
 			// 打开编辑图片面板
 			this.isShowEditPanel = true;
+
+			// 打开选择文件框
+			this.myCroppa.chooseFile();
+
+			// 计算 croppa 宽高和缩放比例
+			this.computeCroppaSize();
 		},
 		// 使用图片
 		useImage() {
 			// 关闭编辑图片面板
 			this.isShowEditPanel = false;
 
-			// 存放展示图片的链接 blob 和上传图片的链接 base64
-			// to do
+			// 拦截器：防止没有选择图片就对 croppa 对象操作导致错误
+			if (this.myCroppa.imageSet) {
+				// 存放展示图片的链接 blob 和上传图片的链接 base64
+				// 获取当前正在编辑的模板数组
+				var templateEditingObject = this[`templateData${this.templateType}`];
+				// 生成 base64 字符串
+				var base64Url = this.myCroppa.generateDataUrl('image/jpeg', 0.8);
+				// 在模板数据对象中匹配到正在编辑的对象
+				var imageEditingObject = templateEditingObject[_.findKey(templateEditingObject, { 'imageId': this.imageEditing.imageId })];
+				// 生成 blob 地址并传入模板数据对象的展示位置
+				this.myCroppa.generateBlob((blob) => {
+					var url = URL.createObjectURL(blob);
+					imageEditingObject.initImage = url;
+				}, 'image/jpeg', 0.8)
 
-			// 判断是否选择完所有图片
-			this[`templateData${this.templateType}`].forEach((item, index)=>{
-				if (item.base64Image == '') {
-					this.isAllowSubmitCard = false;
-					return
-				} else {
-					this.isAllowSubmitCard = true;
+				// 缓存传给后台的 base64 字符串
+				// imageEditingObject.base64Image = base64Url;
+
+				// 改变图片选中状态
+				imageEditingObject.imageChosen = true;
+
+				// 判断是否选择完所有图片
+				templateEditingObject.forEach((item, index) => {
+					if (item.imageChosen == '') {
+						this.isAllowSubmitCard = false;
+						return
+					} else {
+						this.isAllowSubmitCard = true;
+					}
+				});
+
+				// 上传图片到服务器
+				this.$http.post('/model/Work/workpicture', qs.stringify({
+					type: this.templateType,
+					id: this.imageEditing.imageId.split('-')[1],
+					picture: base64Url
+				})).then((response) => {
+					var res = response.data;
+					console.log(res);
+				});
+			}
+
+			// 删除 croppa 中图片
+			this.myCroppa.remove();
+		},
+		// 计算 croppa 宽高和缩放比例
+		computeCroppaSize() {
+			var screenWidth = this.$refs.CardChooseImages.clientWidth,
+				screenHeight = this.$refs.CardChooseImages.clientHeight;
+			if (this.imageEditing.width <= screenWidth && this.imageEditing.height <= screenHeight) {
+				this.croppaWidth = this.imageEditing.width;
+				this.croppaHeight = this.imageEditing.height;
+				this.croppaQuality = 1;
+			} else if (this.imageEditing.width <= screenWidth && this.imageEditing.height > screenHeight) {
+				this.croppaWidth = screenHeight * this.imageEditing.aspectRatio;
+				this.croppaHeight = screenHeight;
+				this.croppaQuality = this.imageEditing.height / screenHeight;
+			} else if (this.imageEditing.width > screenWidth && this.imageEditing.height <= screenHeight) {
+				this.croppaWidth = screenWidth;
+				this.croppaHeight = screenWidth / this.imageEditing.aspectRatio;
+				this.croppaQuality = this.imageEditing.width / screenWidth;
+			} else if (this.imageEditing.width > screenWidth && this.imageEditing.height > screenHeight) {
+				if (this.imageEditing.width / screenWidth > this.imageEditing.height / screenHeight) {
+					this.croppaWidth = screenHeight * this.imageEditing.aspectRatio;
+					this.croppaHeight = screenHeight;
+					this.croppaQuality = this.imageEditing.height / screenHeight;
+				} else if (this.imageEditing.width / screenWidth < this.imageEditing.height / screenHeight) {
+					this.croppaWidth = screenWidth;
+					this.croppaHeight = screenWidth / this.imageEditing.aspectRatio;
+					this.croppaQuality = this.imageEditing.width / screenWidth;
+				} else if (this.imageEditing.width / screenWidth == this.imageEditing.height / screenHeight) {
+					this.croppaWidth = screenWidth;
+					this.croppaHeight = screenHeight;
+					this.croppaQuality = this.imageEditing.width / screenWidth;
 				}
-			});
+			}
 		}
 	},
 	computed: {
-
+		...mapGetters(['modelCardDataGetter'])
 	},
 	mounted() {
-		this.screenWidth = this.$refs.CardChooseImages.clientWidth
+
 	}
 }
 </script>
 <style lang="less" scoped>
 #CardChooseImages {
-	// croppa
+	width: 100%;
+	height: 100%; // croppa
 	.croppa-container {
 		display: block;
-	}
-	// 五图模板
+	} // 五图模板
 	.five-images-template {
 		.header {
 			background: #fe3076;
@@ -367,12 +452,17 @@ export default {
 				}
 			}
 			.main {
+				display: flex;
+				justify-content: center;
+				align-items: center;
 				position: absolute;
 				top: 1.3125rem;
 				bottom: 0;
 				left: 0;
 				right: 0;
-				background-color: #fff;
+				background-color: #fff; // .croppa-container {
+				// 	align-self: auto;
+				// }
 			}
 		}
 
